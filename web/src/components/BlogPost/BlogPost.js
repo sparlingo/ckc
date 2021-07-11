925c29feec464f3e4255bc94f80a21a9904daeeb
Kevin Sparling <kevinsparling@gmail.com>
2021-07-11 16:15:08 +0800

    into tutorial 2

---
+++ web/src/components/BlogPost/BlogPost.js
@@ -0,0 +1,22 @@
+import { Link, routes } from '@redwoodjs/router'
+
+const truncate = (text, length) => {
+  return text.substring(0, length) + '...'
+}
+
+const BlogPost = ({ post, summary = false }) => {
+  return (
+    <article>
+      <header>
+        <h2 className="text-xl text-blue-700 font-semibold">
+          <Link to={routes.blogPost({ id: post.id })}>{post.title}</Link>
+        </h2>
+      </header>
+      <div className="mt-2 text-gray-900 font-light">
+        {summary ? truncate(post.body, 100) : post.body}
+      </div>
+    </article>
+  )
+}
+
+export default BlogPost
---
+++ web/src/components/BlogPostsCell/BlogPostsCell.js
@@ -0,0 +1,28 @@
+import BlogPost from 'src/components/BlogPost'
+
+export const QUERY = gql`
+  query BlogPostsQuery {
+    posts {
+      id
+      title
+      body
+      createdAt
+    }
+  }
+`
+
+export const Loading = () => <div>Loading...</div>
+
+export const Empty = () => <div>Empty</div>
+
+export const Failure = ({ error }) => <div>Error: {error.message}</div>
+
+export const Success = ({ posts }) => {
+  return (
+    <div className="space-y-10">
+      {posts.map((post) => (
+        <BlogPost post={post} summary={true} key={post.id} />
+      ))}
+    </div>
+  )
+}
